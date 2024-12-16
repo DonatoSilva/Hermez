c3377e98c67501a7bbe00eeec77d2042be7eb416
Donato Silva <figueroajesus2015@hotmail.com>
2024-12-15 19:52:44 -0500

    Theme of color config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'H-black': '#0c1218',
+				'H-blue-900': '#2b4055',
+				'H-blue-700': '#496e93',
+				'H-blue-500': '#82a1c1',
+				'H-blue-300': '#bfcfdf',
+				'H-blue-100': '#fdfdfe',
+			},
+		},
 	},
 	plugins: [],
 }
