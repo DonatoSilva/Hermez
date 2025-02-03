374daf3be63f959e59e1003ba5af977916899499
Donato Silva <figueroajesus2015@hotmail.com>
2025-02-03 11:26:19 -0500

    feat: implement authentication middleware and update layout to conditionally render navigation based on login status

---
+++ src/middleware.js
@@ -0,0 +1,30 @@
+import { defineMiddleware } from "astro:middleware"
+import { firebase } from "./firebase/config"
+
+const privateRoutes = ['/domicile/record', '/record']
+const notProtected = ['/domicile/signUp', '/domicile']
+
+export const onRequest = defineMiddleware(
+    async ({ url, locals, redirect }, next) => {
+        const isLoggedIn = !!firebase.auth.currentUser
+        const domicile = firebase.auth.currentUser
+
+        locals.isLoggedIn = isLoggedIn;
+        if (isLoggedIn) {
+            locals.domicile = {
+                email: domicile.email,
+                emailVerified: domicile.emailVerified
+            }
+        }
+
+        if (!isLoggedIn && privateRoutes.includes(url.pathname)) {
+            return redirect('/')
+        }
+
+        if (isLoggedIn && notProtected.includes(url.pathname)) {
+            return redirect('/')
+        }
+
+        return next();
+    }
+)
