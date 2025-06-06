90b33b07e8fb7c43eb5acc3a69f3b2d52bc8b27a
Donato Silva <figueroajesus2015@hotmail.com>
2025-06-05 22:37:05 -0500

    refactor: remove Domicile interface and simplify Locals in env.d.ts; remove delivery verification logic in index.astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,18 +2,7 @@
 /// <reference types="astro/client" />
 
 declare namespace App {
-    interface Domicile {
-        email: string;
-        emailVerified: boolean;
-        displayName: string;
-        uid: string;
-    }
-
-
-    interface Locals {
-        isLoggedIn: boolean;
-        domicile: Domicile | null;
-    }
+    interface Locals { }
 }
 
 interface ImportMetaEnv {
