3d6d3eac611bd3bca3cd9e6dcfcd1cfd62aa6f7b
Donato Silva <figueroajesus2015@hotmail.com>
2025-10-31 15:14:52 -0500

    feat(entrega): agregar componentes del formulario de solicitud de entrega y refactorizar la URL de la API
    - Crear nuevos componentes para los pasos del formulario de solicitud de entrega
    - Extraer el formulario de entrega a un componente independiente BodyForm
    - Actualizar la construcción de la URL de la API en actions para usar variables de entorno
    - Actualizar dependencias a las últimas versiones

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,13 +1,15 @@
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 
+const usersURLAPI = `${import.meta.env.URL_LOCAL_BACKEND}${import.meta.env.API_USERS}`;
+
 export const server = {
     getUserData: defineAction({
         input: z.object({
             id: z.string(),
         }),
         handler: async (input) => {
-            const response = await fetch(`http://127.0.0.1:8000/user/api/users/${input.id}`);
+            const response = await fetch(`${usersURLAPI}${input.id}`);
             const { gender: userGender, phone, age } = await response.json();
             const gender = userGender === 'male' ? 'masculino' : 'femenino';
             return { gender, phone, age };
